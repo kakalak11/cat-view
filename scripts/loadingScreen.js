05f9c5cf1b686465f5db27ae06e89e3a8ea74e0d
kakalak33 <tu.tran@enotion.io>
2024-10-15 21:03:15 +0700

    - use new loading animation

--- scripts/loadingScreen.js
+++ scripts/loadingScreen.js
@@ -1,27 +1,34 @@
 const { div } = van.tags;
 
+// function loadingScreen() {
+//     const _div = div({ id: "loading-scrren-wrapper", class: "wrapper" },
+//         div({ class: "dot" }),
+//         div({ class: "dot" }),
+//         div({ class: "dot" }),
+//         div({ class: "dot" }),
+//         div({ class: "dot" }),
+//     );
+//     const loadingLabel = div({ class: "wrapper loading-label" }, div({ class: "loading-label" }, "LOADING"))
+
+//     let count = 0;
+//     let loopRate = 1000 * 0.2;
+
+//     setInterval(() => {
+//         const childNodes = Array.from(_div.children);
+//         const child = childNodes[count % childNodes.length]; child.classList.toggle("animation-selected");
+//         count++;
+//     }, loopRate);
+
+//     const bgGradientDiv = div({ class: "gradient" }, loadingLabel, _div);
+
+//     return bgGradientDiv;
+// }
+
 function loadingScreen() {
-
-
     const _div = div({ id: "loading-scrren-wrapper", class: "wrapper" },
-        div({ class: "dot" }),
-        div({ class: "dot" }),
-        div({ class: "dot" }),
-        div({ class: "dot" }),
-        div({ class: "dot" }),
-    );
-    const loadingLabel = div({ class: "wrapper loading-label" }, div({ class: "loading-label" }, "LOADING"))
-
-    let count = 0;
-    let loopRate = 1000 * 0.2;
-
-    setInterval(() => {
-        const childNodes = Array.from(_div.children);
-        const child = childNodes[count % childNodes.length]; child.classList.toggle("animation-selected");
-        count++;
-    }, loopRate);
-
-    const bgGradientDiv = div({ class: "gradient" }, loadingLabel, _div);
+        div({ class: "loader" })
+    )
+    const bgGradientDiv = div({ class: "gradient" }, _div);
 
     return bgGradientDiv;
 }
